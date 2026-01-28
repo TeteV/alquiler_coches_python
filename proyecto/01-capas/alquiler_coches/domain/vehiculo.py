4da253a68d566e2aae1b6e63ef885d93792926bd
Nestor C <nestorjoaquincabanillassarmiento@alumno.ieselrincon.es>
2026-01-28 21:25:23 +0000

    Creacion de la estructura por capas , clases y menu simples

---
+++ proyecto/01-capas/alquiler_coches/domain/vehiculo.py
@@ -0,0 +1,39 @@
+class Vehiculo:
+    def __init__(self, matricula, marca, modelo, anio, puertas = 0):
+        self.matricula = matricula
+        self.marca = marca
+        self.modelo = modelo
+        self.anio = anio
+        self.puertas = puertas
+        self.alquilado = False
+
+    @property
+    def matricula(self):
+        return self._matricula
+    @property
+    def anio(self):
+        return self._anio
+    
+    @matricula.setter
+    def matricula(self, value):
+        value = value.strip().upper()
+        if len(value) != 2 or not value[0].isalpha() or not value[1].isdigit():
+            raise ValueError("La matrícula debe tener el formato 'letra+numero'.")
+    @anio.setter
+    def anio(self, value):
+        if value < 1900 or value > 2026:
+            raise ValueError("El año debe estar entre 1900 y 2026.")
+        self._anio = value
+
+    def alquilar(self):
+        if not self.alquilado:
+            self.alquilado = True
+            return "Vehiculo alquilado con éxito."
+        return "El vehículo ya está alquilado."
+    
+    def devolver(self):
+        if self.alquilado:
+            self.alquilado = False
+            return "Vehiculo devuelto con éxito."
+        return "El vehículo no estaba alquilado."
+
---
+++ proyecto/01-capas/alquiler_coches/presentation/menu.py
@@ -0,0 +1,39 @@
+def mostrar_menu():
+    print("1. Opción 1")
+    print("2. Opción 2")
+    print("3. Opción 3")
+    print("4. Opción 4")
+    print("5. Opción 5")
+    print("6. Opción 6")
+    print("7. Opción 7")
+    print("8. Salir")
+
+def main():
+    while True:
+        mostrar_menu()
+        eleccion = input("Seleccione una opción (1-8): ")
+        try:
+            if eleccion == '1':
+                print("Has seleccionado la Opción 1")
+            elif eleccion == '2':
+                print("Has seleccionado la Opción 2")
+            elif eleccion == '3':
+                print("Has seleccionado la Opción 3")
+            elif eleccion == '4':
+                print("Has seleccionado la Opción 4")
+            elif eleccion == '5':
+                print("Has seleccionado la Opción 5")
+            elif eleccion == '6':
+                print("Has seleccionado la Opción 6")
+            elif eleccion == '7':
+                print("Has seleccionado la Opción 7")
+            elif eleccion == '8':
+                print("Saliendo del menú. ¡Hasta luego!")
+                break
+            else:
+                print("Opción no válida. Por favor, intente de nuevo.")
+        except ValueError as e:
+                print("X " + str(e))
+
+if __name__ == "__main__":
+    main()
